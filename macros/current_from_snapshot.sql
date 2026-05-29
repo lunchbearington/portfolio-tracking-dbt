df1ae8ad8077aeca833fd452de433ada7dbda2a7
Leo Helfer <leohelfer0868@gmail.com>
2026-05-29 02:10:43 +0000

    progressed further through chapter

--- macros/current_from_snapshot.sql
+++ macros/current_from_snapshot.sql
@@ -1,7 +1,11 @@
-{% macro current_from_snapshot(snsh_ref) %} 
-SELECT 
-    * EXCLUDE (DBT_SCD_ID, DBT_VALID_FROM, DBT_VALID_TO) RENAME (DBT_UPDATED_AT as SNSH_LOAD_TS_UTC ) 
+{% macro current_from_snapshot(snsh_ref, output_load_ts = true) %}
+ SELECT 
+    * 
+    EXCLUDE (DBT_SCD_ID, DBT_VALID_FROM, DBT_VALID_TO, DBT_UPDATED_AT) {% if output_load_ts %} 
+    , DBT_UPDATED_AT as SNSH_LOAD_TS_UTC 
+    {% endif%} 
 FROM 
-    {{ snsh_ref }}
- WHERE 
-    DBT_VALID_TO is null {% endmacro %} 
+    {{ snsh_ref }} 
+WHERE 
+    DBT_VALID_TO is null 
+{% endmacro %}
