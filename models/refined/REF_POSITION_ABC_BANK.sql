698f9578386da78e2e72e8ae686540ffff9eae6d
Leo Helfer <leohelfer0868@gmail.com>
2026-05-29 01:20:00 +0000

    progressed through the chapter

--- models/refined/REF_POSITION_ABC_BANK.sql
+++ models/refined/REF_POSITION_ABC_BANK.sql
@@ -1,20 +1,15 @@
-WITH current_from_snapshot as
-(
-    SELECT 
-        *
-    FROM 
-        {{ ref('SNSH_ABC_BANK_POSITION') }}
-    WHERE 
-        DBT_VALID_TO is null
-)
-SELECT
-    *
-    , POSITION_VALUE - COST_BASE as UNREALIZED_PROFIT
-    , ROUND(UNREALIZED_PROFIT / COST_BASE,5) as UNREALIZED_PROFIT_PCT
+WITH current_from_snapshot as 
+( 
+    {{ current_from_snapshot( snsh_ref = ref('SNSH_ABC_BANK_POSITION') ) }}
+) 
+SELECT 
+    * 
+    , POSITION_VALUE - COST_BASE as UNREALIZED_PROFIT 
+    , ROUND(UNREALIZED_PROFIT / COST_BASE, 5)*100 as UNREALIZED_PROFIT_PCT 
 FROM 
-    {{ ref('STG_ABC_BANK_POSITION') }}
+    current_from_snapshot
 
-
+ 
 
 
  
